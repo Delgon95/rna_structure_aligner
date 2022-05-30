a5d76adfeea052bb9d8eb12a10929b722999160f
Michal Zurkowski <michal.zurkowski@gmail.com>
2022-05-30 13:52:08 +0200

    Add more examples. Add .cif examples.

--- run_examples.sh
+++ run_examples.sh
@@ -17,72 +17,134 @@ function run_rnahugs() {
 
 }
 
-pushd example/1
+
+
+# Run examples in CIF format
+pushd example/cif/2
+REF="1_solution_0_rpr.cif"
+TARGET="1_das_3_rpr.cif"
+
+echo "Running example 2 cif - Geometric Sequence independent 3.5A"
+run_rnahugs ${REF} ${TARGET} "geometric" "seq-indep" "3.5"
+
+echo "Running example 2 cif - Genetic Sequence independent 3.5A"
+run_rnahugs ${REF} ${TARGET} "genetic" "seq-indep" "3.5"
+
+echo "Running example 2 cif - Geometric Sequence dependent 3.5A"
+run_rnahugs ${REF} ${TARGET} "geometric" "seq-dep" "3.5"
+
+echo "Running example 2 cif - Genetic Sequence dependent 3.5A"
+run_rnahugs ${REF} ${TARGET} "genetic" "seq-dep" "3.5"
+
+popd
+
+pushd example/cif/3
+REF="4_0_solution_3V7E_rpr.cif"
+TARGET="4_chen_1_rpr.cif"
+
+echo "Running example 3 cif - Geometric Sequence independent 3.5A"
+run_rnahugs ${REF} ${TARGET} "geometric" "seq-indep" "3.5"
+
+echo "Running example 3 cif - Genetic Sequence independent 3.5A"
+run_rnahugs ${REF} ${TARGET} "genetic" "seq-indep" "3.5"
+
+echo "Running example 3 cif - Geometric Sequence dependent 3.5A"
+run_rnahugs ${REF} ${TARGET} "geometric" "seq-dep" "3.5"
+
+echo "Running example 3 cif - Genetic Sequence dependent 3.5A"
+run_rnahugs ${REF} ${TARGET} "genetic" "seq-dep" "3.5"
+
+popd
+
+pushd example/cif/4
+REF="3_solution_0_rpr.cif"
+TARGET="3_chen_1_rpr.cif"
+
+echo "Running example 4 cif - Geometric Sequence independent 3.5A"
+run_rnahugs ${REF} ${TARGET} "geometric" "seq-indep" "3.5"
+
+echo "Running example 4 cif - Genetic Sequence independent 3.5A"
+run_rnahugs ${REF} ${TARGET} "genetic" "seq-indep" "3.5"
+
+echo "Running example 4 cif - Geometric Sequence dependent 3.5A"
+run_rnahugs ${REF} ${TARGET} "geometric" "seq-dep" "3.5"
+
+echo "Running example 4 cif - Genetic Sequence dependent 3.5A"
+run_rnahugs ${REF} ${TARGET} "genetic" "seq-dep" "3.5"
+
+popd
+
+# Run examples in PDB format
+pushd example/pdb/1
 REF="1_solution_0A_rpr.pdb"
 TARGET="testA_rpr.pdb"
 
-echo "Running example 1 - Geometric Sequence independent 3.5A"
+echo "Running example 1 pdb - Geometric Sequence independent 3.5A"
 run_rnahugs ${REF} ${TARGET} "geometric" "seq-indep" "3.5"
 
-echo "Running example 1 - Genetic Sequence independent 3.5A"
+echo "Running example 1 pdb - Genetic Sequence independent 3.5A"
 run_rnahugs ${REF} ${TARGET} "genetic" "seq-indep" "3.5"
 
-echo "Running example 1 - Geometric Sequence dependent 3.5A"
+echo "Running example 1 pdb - Geometric Sequence dependent 3.5A"
 run_rnahugs ${REF} ${TARGET} "geometric" "seq-dep" "3.5"
 
-echo "Running example 1 - Genetic Sequence dependent 3.5A"
+echo "Running example 1 pdb - Genetic Sequence dependent 3.5A"
 run_rnahugs ${REF} ${TARGET} "genetic" "seq-dep" "3.5"
 
 popd
 
-pushd example/2
+pushd example/pdb/2
 REF="1_solution_0_rpr.pdb"
 TARGET="1_das_3_rpr.pdb"
 
-echo "Running example 2 - Geometric Sequence independent 3.5A"
+echo "Running example 2 pdb - Geometric Sequence independent 3.5A"
 run_rnahugs ${REF} ${TARGET} "geometric" "seq-indep" "3.5"
 
-echo "Running example 2 - Genetic Sequence independent 3.5A"
+echo "Running example 2 pdb - Genetic Sequence independent 3.5A"
 run_rnahugs ${REF} ${TARGET} "genetic" "seq-indep" "3.5"
 
-echo "Running example 2 - Geometric Sequence dependent 3.5A"
+echo "Running example 2 pdb - Geometric Sequence dependent 3.5A"
 run_rnahugs ${REF} ${TARGET} "geometric" "seq-dep" "3.5"
 
-echo "Running example 2 - Genetic Sequence dependent 3.5A"
+echo "Running example 2 pdb - Genetic Sequence dependent 3.5A"
 run_rnahugs ${REF} ${TARGET} "genetic" "seq-dep" "3.5"
 
 popd
 
-pushd example/3
+
+pushd example/pdb/3
 REF="4_0_solution_3V7E_rpr.pdb"
 TARGET="4_chen_1_rpr.pdb"
 
-echo "Running example 3 - Geometric Sequence independent 3.5A"
+echo "Running example 3 pdb - Geometric Sequence independent 3.5A"
 run_rnahugs ${REF} ${TARGET} "geometric" "seq-indep" "3.5"
 
-echo "Running example 3 - Genetic Sequence independent 3.5A"
+echo "Running example 3 pdb - Genetic Sequence independent 3.5A"
 run_rnahugs ${REF} ${TARGET} "genetic" "seq-indep" "3.5"
 
-echo "Running example 3 - Geometric Sequence dependent 3.5A"
+echo "Running example 3 pdb - Geometric Sequence dependent 3.5A"
 run_rnahugs ${REF} ${TARGET} "geometric" "seq-dep" "3.5"
 
-echo "Running example 3 - Genetic Sequence dependent 3.5A"
+echo "Running example 3 pdb - Genetic Sequence dependent 3.5A"
 run_rnahugs ${REF} ${TARGET} "genetic" "seq-dep" "3.5"
 
 popd
 
-pushd example/4
+pushd example/pdb/4
 REF="3_solution_0_rpr.pdb"
 TARGET="3_chen_1_rpr.pdb"
 
-echo "Running example 4 - Geometric Sequence independent 3.5A"
+echo "Running example 4 pdb - Geometric Sequence independent 3.5A"
 run_rnahugs ${REF} ${TARGET} "geometric" "seq-indep" "3.5"
 
-echo "Running example 4 - Genetic Sequence independent 3.5A"
+echo "Running example 4 pdb - Genetic Sequence independent 3.5A"
 run_rnahugs ${REF} ${TARGET} "genetic" "seq-indep" "3.5"
 
-echo "Running example 4 - Geometric Sequence dependent 3.5A"
+echo "Running example 4 pdb - Geometric Sequence dependent 3.5A"
 run_rnahugs ${REF} ${TARGET} "geometric" "seq-dep" "3.5"
 
-echo "Running example 4 - Genetic Sequence dependent 3.5A"
+echo "Running example 4 pdb - Genetic Sequence dependent 3.5A"
 run_rnahugs ${REF} ${TARGET} "genetic" "seq-dep" "3.5"
+
+popd
+

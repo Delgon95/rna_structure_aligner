99674db1939f2af2603695624fa8e0d104131240
Michal Zurkowski <michal.zurkowski@gmail.com>
2023-03-10 04:17:23 +0100

    Add respect-order examples. Improve examples script.

--- run_examples.sh
+++ run_examples.sh
@@ -9,144 +9,44 @@ if [[ ! -f ${JAR} ]]; then
 fi
 
 function run_rnahugs() {
-
   FOLDER="${3}_${4}"
+  if [[ ${6} ]]; then
+    FOLDER="${FOLDER}_${6}"
+  fi
   rm -r "${FOLDER}" 2>/dev/null ; mkdir "${FOLDER}" 2>/dev/null
-  java -jar ${JAR} -r ${REF} -t ${TARGET} --method ${3} --mode ${4} --rmsd ${5} -o "${FOLDER}"
+  COMMAND="java -Xms256m -Xmx8g -jar ${JAR} -r ${REF} -t ${TARGET} --method ${3} --mode ${4} --rmsd ${5} -o ${FOLDER}"
+  if [[ ${6} ]]; then
+    COMMAND="${COMMAND} --respect-order"
+  fi
+
+  echo "Running \"${COMMAND}\""
+  ${COMMAND}
+
   cat ${FOLDER}/*-output.txt ; echo
   cat ${FOLDER}/*sequence-alignment.txt ; echo
 
 }
 
 
-
-# Run examples in CIF format
-pushd example/cif/2
-REF="1_solution_0_rpr.cif"
-TARGET="1_das_3_rpr.cif"
-
-echo "Running example 2 cif - Geometric Sequence independent 3.5A"
-run_rnahugs ${REF} ${TARGET} "geometric" "seq-indep" "3.5"
-
-echo "Running example 2 cif - Genetic Sequence independent 3.5A"
-run_rnahugs ${REF} ${TARGET} "genetic" "seq-indep" "3.5"
-
-echo "Running example 2 cif - Geometric Sequence dependent 3.5A"
-run_rnahugs ${REF} ${TARGET} "geometric" "seq-dep" "3.5"
-
-echo "Running example 2 cif - Genetic Sequence dependent 3.5A"
-run_rnahugs ${REF} ${TARGET} "genetic" "seq-dep" "3.5"
-
-popd
-
-
-pushd example/cif/3
-REF="4_0_solution_3V7E_rpr.cif"
-TARGET="4_chen_1_rpr.cif"
-
-echo "Running example 3 cif - Geometric Sequence independent 3.5A"
-run_rnahugs ${REF} ${TARGET} "geometric" "seq-indep" "3.5"
-
-echo "Running example 3 cif - Genetic Sequence independent 3.5A"
-run_rnahugs ${REF} ${TARGET} "genetic" "seq-indep" "3.5"
-
-echo "Running example 3 cif - Geometric Sequence dependent 3.5A"
-run_rnahugs ${REF} ${TARGET} "geometric" "seq-dep" "3.5"
-
-echo "Running example 3 cif - Genetic Sequence dependent 3.5A"
-run_rnahugs ${REF} ${TARGET} "genetic" "seq-dep" "3.5"
-
-popd
-
-pushd example/cif/4
-REF="3_solution_0_rpr.cif"
-TARGET="3_chen_1_rpr.cif"
-
-echo "Running example 4 cif - Geometric Sequence independent 3.5A"
-run_rnahugs ${REF} ${TARGET} "geometric" "seq-indep" "3.5"
-
-echo "Running example 4 cif - Genetic Sequence independent 3.5A"
-run_rnahugs ${REF} ${TARGET} "genetic" "seq-indep" "3.5"
-
-echo "Running example 4 cif - Geometric Sequence dependent 3.5A"
-run_rnahugs ${REF} ${TARGET} "geometric" "seq-dep" "3.5"
-
-echo "Running example 4 cif - Genetic Sequence dependent 3.5A"
-run_rnahugs ${REF} ${TARGET} "genetic" "seq-dep" "3.5"
-
-popd
-
-# Run examples in PDB format
-pushd example/pdb/1
-REF="1_solution_0A_rpr.pdb"
-TARGET="testA_rpr.pdb"
-
-echo "Running example 1 pdb - Geometric Sequence independent 3.5A"
-run_rnahugs ${REF} ${TARGET} "geometric" "seq-indep" "3.5"
-
-echo "Running example 1 pdb - Genetic Sequence independent 3.5A"
-run_rnahugs ${REF} ${TARGET} "genetic" "seq-indep" "3.5"
-
-echo "Running example 1 pdb - Geometric Sequence dependent 3.5A"
-run_rnahugs ${REF} ${TARGET} "geometric" "seq-dep" "3.5"
-
-echo "Running example 1 pdb - Genetic Sequence dependent 3.5A"
-run_rnahugs ${REF} ${TARGET} "genetic" "seq-dep" "3.5"
-
-popd
-
-pushd example/pdb/2
-REF="1_solution_0_rpr.pdb"
-TARGET="1_das_3_rpr.pdb"
-
-echo "Running example 2 pdb - Geometric Sequence independent 3.5A"
-run_rnahugs ${REF} ${TARGET} "geometric" "seq-indep" "3.5"
-
-echo "Running example 2 pdb - Genetic Sequence independent 3.5A"
-run_rnahugs ${REF} ${TARGET} "genetic" "seq-indep" "3.5"
-
-echo "Running example 2 pdb - Geometric Sequence dependent 3.5A"
-run_rnahugs ${REF} ${TARGET} "geometric" "seq-dep" "3.5"
-
-echo "Running example 2 pdb - Genetic Sequence dependent 3.5A"
-run_rnahugs ${REF} ${TARGET} "genetic" "seq-dep" "3.5"
-
-popd
-
-
-pushd example/pdb/3
-REF="4_0_solution_3V7E_rpr.pdb"
-TARGET="4_chen_1_rpr.pdb"
-
-echo "Running example 3 pdb - Geometric Sequence independent 3.5A"
-run_rnahugs ${REF} ${TARGET} "geometric" "seq-indep" "3.5"
-
-echo "Running example 3 pdb - Genetic Sequence independent 3.5A"
-run_rnahugs ${REF} ${TARGET} "genetic" "seq-indep" "3.5"
-
-echo "Running example 3 pdb - Geometric Sequence dependent 3.5A"
-run_rnahugs ${REF} ${TARGET} "geometric" "seq-dep" "3.5"
-
-echo "Running example 3 pdb - Genetic Sequence dependent 3.5A"
-run_rnahugs ${REF} ${TARGET} "genetic" "seq-dep" "3.5"
-
-popd
-
-pushd example/pdb/4
-REF="3_solution_0_rpr.pdb"
-TARGET="3_chen_1_rpr.pdb"
-
-echo "Running example 4 pdb - Geometric Sequence independent 3.5A"
-run_rnahugs ${REF} ${TARGET} "geometric" "seq-indep" "3.5"
-
-echo "Running example 4 pdb - Genetic Sequence independent 3.5A"
-run_rnahugs ${REF} ${TARGET} "genetic" "seq-indep" "3.5"
-
-echo "Running example 4 pdb - Geometric Sequence dependent 3.5A"
-run_rnahugs ${REF} ${TARGET} "geometric" "seq-dep" "3.5"
-
-echo "Running example 4 pdb - Genetic Sequence dependent 3.5A"
-run_rnahugs ${REF} ${TARGET} "genetic" "seq-dep" "3.5"
-
-popd
-
+for TYPE in pdb cif; do 
+  for EXAMPLE in {1..4}; do
+    # Check if the directory exists. Example 1 in cif is missing.
+    EX_FOLDER="example/${TYPE}/${EXAMPLE}"
+    if [[ -d ${EX_FOLDER} ]]; then
+      FILES=$(find ${EX_FOLDER} -maxdepth 1 -iname "*.${TYPE}" | xargs -L1 basename --)
+      REF=$(echo ${FILES} | tr " " "\n" | grep "solution")
+      TARGET=$(echo ${FILES} | tr " " "\n" | grep -v "solution")
+      
+      pushd ${EX_FOLDER}
+      for ORDER in "" respect-order; do
+        for MODE in seq-indep seq-dep; do
+          for METHOD in geometric genetic; do
+            echo "Running example ${EXAMPLE} ${TYPE} - ${METHOD} ${MODE} ${ORDER} 3.5A"
+            run_rnahugs ${REF} ${TARGET} ${METHOD} ${MODE} "3.5" ${ORDER}
+          done
+        done
+      done
+      popd
+    fi
+  done
+done
